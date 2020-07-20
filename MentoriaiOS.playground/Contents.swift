c49524b0ea4f42194e70e61b5ea9d59958fcd1b4
jean.santos <jean.santos@bancointer.com.br>
2020-07-20 11:04:45 -0300

    Validated if the password has at least one capital letter.

--- MentoriaiOS.playground/Contents.swift
+++ MentoriaiOS.playground/Contents.swift
@@ -1,11 +1,38 @@
 import Foundation
+import UIKit
 
-let passwordsToValidate: [String]
-passwordsToValidate = ["#forTe1", "senhafraca", "Qu@s1", "Voce@Consegue!2019"]
+/*
+ no mínimo 7 caracteres - ok
+ pelo menos uma letra maiúscula - ok
+ pelo menos uma letra minúscula
+*/
+
+/*
+ uma lista de palavras
+ pegar uma palavra
+ verificar se tem letra maiúscula nesta palavra
+*/
+
+var passwordsToValidate = ["#forTe1", "senhafraca", "Qu@s1", "Voce@Consegue!2019"]
 
 private func validatePassword(with password: [String]) -> [String] {
-
+    
     return passwordsToValidate.filter { $0.count >= 7 }
 }
 
 validatePassword(with: passwordsToValidate)
+
+//
+var newPasswords = [String]()
+
+for password in passwordsToValidate {
+    for character in password {
+        if character.isUppercase {
+            newPasswords.append(password)
+            break
+        }
+    }
+}
+
+print("Resultados")
+print(newPasswords)
